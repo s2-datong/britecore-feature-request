46cdffc16b6f23a78c29c28f5f163778b9f42cd8
samwul <datong.samwul@gmail.com>
2018-11-28 16:52:21 +0100

    oh no! line going over the edge. Fix

--- quiz.py
+++ quiz.py
@@ -3,7 +3,10 @@ from cryptography.fernet import Fernet
 key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
 
 # Oh no! The code is going over the edge! What are you going to do?
-message = b'gAAAAABb9sIQ64Am7MbBopmSDx91B7kekigGppBkRjjKHFc7Bo-tkV_fIodZJLh02Am3Z34o-8RLmGMgKvbnZFTsVJ9EUKNSsCy8X0EqXLsoPq5XPE_Nc34wIGOtdYAIDTcTNyMnMjcggRyR0wLke4uVBV6-0US6HHFUvhQGyyt9ItEthw357_2AEasCA5h0124OOc9E2-2K'
+message = b'''gAAAAABb9sIQ64Am7MbBopmSDx91B7kekigGppBkRjjKHFc7Bo-tkV_
+fIodZJLh02Am3Z34o-8RLmGMgKvbnZFTsVJ9EUKNSsCy8X0EqXLsoPq5XPE_Nc34wIGOtd
+YAIDTcTNyMnMjcggRyR0wLke4uVBV6-0US6HHFUvhQGyyt9ItEthw357_
+2AEasCA5h0124OOc9E2-2K'''
 
 def main():
     print('running main')
